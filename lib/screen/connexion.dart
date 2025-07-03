fea402bd2b67979e8b659cac9b2782f5039cc217
Rina-ui <marinagracia895@gmail.com>
2025-07-03 18:16:22 +0200

    appel e l'api dans login

--- lib/screen/connexion.dart
+++ lib/screen/connexion.dart
@@ -1,7 +1,8 @@
 import 'package:flutter/material.dart';
 import 'package:todo_flutter_app/screen/home.dart';
-
 import 'inscription.dart';
+import 'package:http/http.dart' as http;
+import 'dart:convert';
 
 class Connexion extends StatefulWidget {
   const Connexion({super.key});
@@ -13,6 +14,71 @@ class Connexion extends StatefulWidget {
 class _ConnexionState extends State<Connexion> {
   TextEditingController usernameController = TextEditingController();
   TextEditingController passwordController = TextEditingController();
+
+  //function to login
+  Future<void> login() async{
+    final url = Uri.parse('http://10.0.2.2:3000/api/user/login');
+    final response = await http.post(
+      url,
+      headers: {'Content-Type': 'application/json'},
+      body: jsonEncode({
+        'username': usernameController.text,
+        'password': passwordController.text,
+      }),
+    );
+
+    if(response.statusCode == 200){
+      final data = jsonDecode(response.body);
+      print('Login successful: $data');
+
+      Navigator.push(
+          context,
+          MaterialPageRoute(builder: (context) => Home())
+      );
+    }else if (response.statusCode == 404) {
+      //if user not create his account
+      showDialog(
+          context: context,
+          builder: (context) => AlertDialog(
+            title: Text('User not found'),
+            content: Text('User not found. Please create your account'),
+            actions: [
+              TextButton(
+                  onPressed: () => Navigator.pop(context),
+                  child: Text('Cancel')
+              ),
+
+              TextButton(
+                onPressed: () {
+                  Navigator.push(
+                    context,
+                    MaterialPageRoute(builder: (context) => Inscription()),
+                  );
+                },
+                child: Text('Register'),
+              )
+
+            ]
+          )
+      );
+    }
+    else{
+      print('Login failed: ${response.statusCode}');
+      showDialog(
+          context: context,
+          builder: (context) => AlertDialog(
+            title: Text('Login failed'),
+            content: Text('Login failed. Please check your username and password and verified yours informations'),
+            actions: [
+              TextButton(
+                  onPressed: () => Navigator.pop(context),
+                  child: Text('OK')
+              )
+            ],
+          )
+      );
+    }
+  }
 
   @override
   Widget build(BuildContext context) {
