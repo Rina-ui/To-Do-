ebfb8a107230a6ecf32b1e083e20122f8c7847a0
Rina-ui <marinagracia895@gmail.com>
2025-07-03 13:36:02 +0200

    mise en page de la page login

--- lib/screen/connexion.dart
+++ lib/screen/connexion.dart
@@ -17,81 +17,94 @@ class _ConnexionState extends State<Connexion> {
   Widget build(BuildContext context) {
     return Scaffold(
       body: SafeArea(
-          child: Padding(
+          child: Container(
+            decoration: BoxDecoration(
+                gradient: LinearGradient(
+                    colors: [
+                      Color(0xFFcaf0f8),
+                      Color(0xFFFFFFFF),
+                      Color(0xFFcaf0f8),
+                    ],
+                    begin: Alignment.topRight,
+                    end: Alignment.bottomLeft
+                )
+            ),
+            child: Padding(
               padding: EdgeInsets.all(20),
-            child: Column(
-              mainAxisAlignment: MainAxisAlignment.center,
-              children: [
-                Text("Login",
-                  style: TextStyle(
-                    fontSize: 40,
-                    fontWeight: FontWeight.w500,
-                    letterSpacing: 0,
-                    color: Colors.black,
-                    fontFamily: 'Poppins',
-                    fontStyle: FontStyle.italic
-                  ),
-                ),
-                SizedBox(height: 20),
-                TextField(
-                  controller: usernameController,
-                  decoration: InputDecoration(
-                    hintText: "Username",
-                    border: OutlineInputBorder(
-                      borderRadius: BorderRadius.circular(20),
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  Text("Login",
+                    style: TextStyle(
+                        fontSize: 40,
+                        fontWeight: FontWeight.w500,
+                        letterSpacing: 0,
+                        color: Colors.black,
+                        fontFamily: 'Poppins',
+                        fontStyle: FontStyle.italic
                     ),
                   ),
-                ),
-                SizedBox(height: 20),
-                TextField(
-                  controller: passwordController,
-                  obscureText: true,
-                  decoration: InputDecoration(
-                    hintText: "Password",
-                    border: OutlineInputBorder(
-                      borderRadius: BorderRadius.circular(20),
+                  SizedBox(height: 20),
+                  TextField(
+                    controller: usernameController,
+                    decoration: InputDecoration(
+                      hintText: "Username",
+                      border: OutlineInputBorder(
+                        borderRadius: BorderRadius.circular(20),
+                      ),
                     ),
                   ),
-                ),
+                  SizedBox(height: 20),
+                  TextField(
+                    controller: passwordController,
+                    obscureText: true,
+                    decoration: InputDecoration(
+                      hintText: "Password",
+                      border: OutlineInputBorder(
+                        borderRadius: BorderRadius.circular(20),
+                      ),
+                    ),
+                  ),
 
-                SizedBox(height: 20,),
+                  SizedBox(height: 20,),
 
-                ElevatedButton(
-                    onPressed: () => {
-                      Navigator.push(
-                          context,
-                          MaterialPageRoute(builder: (context) => Connexion())
-                      )
-                    },
-                    child: Text("Login")
-                ),
-
-                SizedBox(height: 10,),
-
-                Row(
-                  mainAxisAlignment: MainAxisAlignment.center,
-                  children: [
-                    Text("Don't have an account?"),
-                    SizedBox(width: 5,),
-                    GestureDetector(
-                      onTap: () =>
-                      {
+                  ElevatedButton(
+                      onPressed: () => {
                         Navigator.push(
                             context,
-                            MaterialPageRoute(builder: (context) => Inscription())
+                            MaterialPageRoute(builder: (context) => Connexion())
                         )
                       },
-                      child: Text("Register",
-                        style: TextStyle(
-                          fontWeight: FontWeight.bold,
-                          color: Colors.blueAccent,
-                        ),
-                      )
-                    )
-                  ],
-                )
+                      child: Text("Login")
+                  ),
 
-              ],
+                  SizedBox(height: 10,),
+
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: [
+                      Text("Don't have an account?"),
+                      SizedBox(width: 5,),
+                      GestureDetector(
+                          onTap: () =>
+                          {
+                            Navigator.push(
+                                context,
+                                MaterialPageRoute(builder: (context) => Inscription())
+                            )
+                          },
+                          child: Text("Register",
+                            style: TextStyle(
+                              fontWeight: FontWeight.bold,
+                              color: Colors.blueAccent,
+                            ),
+                          )
+                      )
+                    ],
+                  )
+
+                ],
+              ),
             ),
           )
       ),
