4c5a20d3fae499c8bccf9ccba6f6163b2dcc70cf
Rina-ui <marinagracia895@gmail.com>
2025-07-07 18:49:03 +0200

    mise en place de la fonction getTasks

--- lib/screen/home.dart
+++ lib/screen/home.dart
@@ -1,7 +1,10 @@
-import 'dart:io';
-
+import 'dart:convert';
+import 'package:http/http.dart' as http;
 import 'package:flutter/material.dart';
+import 'package:shared_preferences/shared_preferences.dart';
 import 'package:todo_flutter_app/screen/screenTask.dart';
+
+import '../pages/task.dart';
 
 class Home extends StatefulWidget {
   const Home({super.key});
@@ -11,6 +14,28 @@ class Home extends StatefulWidget {
 }
 
 class _HomeState extends State<Home> {
+
+  Future<List<Task>> getTasks() async {
+    final prefs = await SharedPreferences.getInstance();
+    final token = prefs.getString('jwt_token') ?? '';
+    final url = Uri.parse('http://10.0.2.2:3000/api/task');
+
+    final response = await http.get(
+      url,
+      headers: {
+        'Content-Type': 'application/json',
+        'Authorization': 'Bearer $token',
+      }
+    );
+
+    if (response.statusCode == 200 || response.statusCode == 201) {
+      final List jsonList = jsonDecode(response.body) as List;
+      // Convert the JSON list to a list of Task objects
+      return jsonList.map((e) => Task.fromJson(e)).toList();
+    }else {
+      throw Exception('Failed to load tasks');
+    }
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -38,6 +63,55 @@ class _HomeState extends State<Home> {
                   )
                 },
                 child: Text('Add a task')
+            ),
+
+            SizedBox(height: 20),
+
+            Expanded(
+                child: FutureBuilder<List<Task>>(
+                  future: getTasks(),
+                  builder: (context, snapshot) {
+                    if (snapshot.connectionState == ConnectionState.waiting) {
+                      return Center(
+                        child: CircularProgressIndicator(),
+                      );
+                    }
+                    if (snapshot.hasError) {
+                      return Center(
+                        child: Text('Error: ${snapshot.error}'),
+                      );
+                    }
+                    final tasks = snapshot.data!;
+                    if (tasks.isEmpty) {
+                      return Center(
+                        child: Text('Nothing task'),
+                      );
+                    }
+                    return ListView.builder(
+                        itemCount: tasks.length,
+                        itemBuilder: (context, index) {
+                          final task = tasks[index];
+                          return ListTile(
+                            title: Text(task.title),
+                            subtitle: Text(task.description),
+                            trailing: Row(
+                              mainAxisSize: MainAxisSize.min,
+                              children: [
+                                IconButton(
+                                    onPressed: () => {},
+                                    icon: Icon(Icons.edit)
+                                ),
+                                IconButton(
+                                    onPressed: () => {},
+                                    icon: Icon(Icons.delete)
+                                )
+                              ],
+                            )
+                          );
+                        }
+                    );
+                  },
+                )
             )
           ],
         ),
